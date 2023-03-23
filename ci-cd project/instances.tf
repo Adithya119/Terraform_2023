54c0673319bfaeb3b577f73ae6fa0e85b8c58a59
Adi <kariveda11@gmail.com>
2023-03-23 12:30:38 +0530

    trying cloud-init for the 1st time

--- ci-cd project/instances.tf
+++ ci-cd project/instances.tf
@@ -7,14 +7,19 @@ data "aws_key_pair" "key-1" {    # pulling existing key-pair --> hence used "dat
     }
 }
 
+data "template_file" "cloud-init-user-data" {
+    template = file("cloud-init.yaml")
+}
+
 resource "aws_instance" "web-vms" {
     count = 2
     ami = "ami-0f8ca728008ff5af4"   # ubuntu 22
     instance_type = "t2.micro"
     associate_public_ip_address = true
-    key_name = data.aws_key_pair.key-1.key_name   # since we are referring "data", use data.aws_key_pair  * * * * * 
+    key_name = data.aws_key_pair.key-1.key_name             # since we are referring "data", use data.aws_key_pair  * * * * * 
     vpc_security_group_ids = [aws_security_group.SG-1.id]
     subnet_id = aws_subnet.subnets-cicd[count.index].id     # tricky [count.index].id       # ids --> plural
+    user_data = data.template_file.cloud-init-user-data.rendered     # user_data
     tags = {
         name = var.vm-names[count.index]   # var in tags as well --> vars can be used anywhere you want
     }    
