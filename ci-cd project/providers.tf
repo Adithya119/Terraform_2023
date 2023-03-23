81105ea470694cf0805712683a644aa3f0cb67eb
Adi <kariveda11@gmail.com>
2023-03-23 12:34:49 +0530

    trying cloud-init for the 1st time

--- ci-cd project/providers.tf
+++ ci-cd project/providers.tf
@@ -1,8 +1,14 @@
 terraform {
   required_providers {
+    
     aws = {
       source = "hashicorp/aws"
       version = "4.59.0"
+    }
+    
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
     }
   }
 }
