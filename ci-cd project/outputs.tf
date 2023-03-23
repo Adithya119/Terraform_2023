368db0b28655e2b07d605667f911ccbaa2a25ca9
Adi <kariveda11@gmail.com>
2023-03-23 12:02:46 +0530

    minor fixes

--- ci-cd project/outputs.tf
+++ ci-cd project/outputs.tf
@@ -1,17 +1,17 @@
 output "vpc_name" {
-    value = aws_vpc.vpc-cicd-1.tags_all
+    value = aws_vpc.vpc-cicd-1.tags
 }
 
 output "SG-name" {
-    value = aws_security_group.SG-1.tags_all
+    value = aws_security_group.SG-1.tags
 }
 
 output "igw-name" {
-    value = aws_internet_gateway.igw-for-cicd.tags_all
+    value = aws_internet_gateway.igw-for-cicd.tags
 }
 
 output "public-rt-name" {
-    value = aws_route_table.public_rt_cicd.tags_all
+    value = aws_route_table.public_rt_cicd.tags
 }
 
 output "vm-1-public-ip" {
@@ -19,5 +19,5 @@ output "vm-1-public-ip" {
 }
 
 output "vm-1-tags" {
-    value = aws_instance.web-vms[0].tags_all
+    value = aws_instance.web-vms[0].tags
 }
