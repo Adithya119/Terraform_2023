ee73c5026cb9c599993da5995bfd3dfba9d49919
Adi <kariveda11@gmail.com>
2023-03-26 15:36:50 +0530

    cloud-config for ansible controller

--- ci-cd project/instances.tf
+++ ci-cd project/instances.tf
@@ -11,7 +11,7 @@ data "aws_key_pair" "key-1" {    # pulling existing key-pair --> hence used "dat
 data "template_cloudinit_config" "cloud-init-user-data" {    # this was working even with just --> data "cloudinit_config"
     part {
       content_type = "text/cloud-config"   # This is "required", opposed to what's mentioned in terraform registry   * * * 
-      content = file("cloud-config.yaml")   
+      content = file("ansible-controller-cloud-config.yml")   
     # content = file("${path.module}/cloud-config.yaml") -->  ${path.module} points to current directory & it is not mandatory
   }
 }
@@ -20,16 +20,16 @@ data "template_cloudinit_config" "cloud-init-user-data" {    # this was working 
 #   https://www.sammeechward.com/cloud-init-and-terraform-with-aws
 
 
-resource "aws_instance" "web-vms" {
-    count = 2
+resource "aws_instance" "ansible-controller" {
     ami = "ami-0f8ca728008ff5af4"   # ubuntu 22
-    instance_type = "t2.micro"
+    instance_type = "t2.medium"
     associate_public_ip_address = true
     key_name = data.aws_key_pair.key-1.key_name             # since we are referring "data", use data.aws_key_pair  * * * * * 
     vpc_security_group_ids = [aws_security_group.SG-1.id]
     subnet_id = aws_subnet.subnets-cicd[count.index].id     # tricky [count.index].id       # ids --> plural
     user_data = data.template_cloudinit_config.cloud-init-user-data.rendered     # user_data
     tags = {
-        name = var.vm-names[count.index]   # var in tags as well --> vars can be used anywhere you want
+        name = "ansible-controller"
+        #name = var.vm-names[count.index]   # var in tags as well --> vars can be used anywhere you want
     }    
 }
